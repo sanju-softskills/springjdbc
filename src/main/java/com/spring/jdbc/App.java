688dad89c31927a1fbf0c230b09c5082bcf89374
Sanjita <sanjitaraj85@gmail.com>
2020-10-27 01:21:09 +0530

    Inserting data way1 to database using Spring JDBC

--- src/main/java/com/spring/jdbc/App.java
+++ src/main/java/com/spring/jdbc/App.java
@@ -1,5 +1,9 @@
 package com.spring.jdbc;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "PRogram started..." );
+        ApplicationContext context = new ClassPathXmlApplicationContext("com/spring/jdbc/config1.xml");
+        JdbcTemplate template = context.getBean("jdbcTemplate",JdbcTemplate.class);
+        String q="insert into student(id,name,city) values(?,?,?)";
+        int result=template.update(q,118,"radha","noida");
+        System.out.println(result); 
     }
 }
