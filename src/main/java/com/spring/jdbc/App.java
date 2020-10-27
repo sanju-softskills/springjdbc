6ea005a6e133084e6c5d6b1b05030b1ad458825b
Sanjita <sanjitaraj85@gmail.com>
2020-10-27 17:04:53 +0530

    insert and update implementation

--- src/main/java/com/spring/jdbc/App.java
+++ src/main/java/com/spring/jdbc/App.java
@@ -4,19 +4,26 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-/**
- * Hello world!
- *
- */
+import com.spring.jdbc.dao.StudentDao;
+import com.spring.jdbc.entites.Student;
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "PRogram started..." );
         ApplicationContext context = new ClassPathXmlApplicationContext("com/spring/jdbc/config1.xml");
-        JdbcTemplate template = context.getBean("jdbcTemplate",JdbcTemplate.class);
-        String q="insert into student(id,name,city) values(?,?,?)";
-        int result=template.update(q,118,"radha","noida");
-        System.out.println(result); 
+		/*
+		 * JdbcTemplate template = context.getBean("jdbcTemplate",JdbcTemplate.class);
+		 * String q="insert into student(id,name,city) values(?,?,?)"; int
+		 * result=template.update(q,118,"radha","noida"); System.out.println(result);
+		 */
+        StudentDao st = context.getBean("studentDao",StudentDao.class);
+        Student s = new Student();
+        s.setId(11);
+        s.setName("qwerty");
+       // st.insert(s);
+       st.change(s);
+       
     }
 }
---
+++ src/main/java/com/spring/jdbc/dao/StudentDaoImpl.java
@@ -0,0 +1,27 @@
+package com.spring.jdbc.dao;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.spring.jdbc.entites.Student;
+
+public class StudentDaoImpl implements StudentDao{
+    private JdbcTemplate template;
+	public JdbcTemplate getTemplate() {
+		return template;
+	}
+	public void setTemplate(JdbcTemplate template) {
+		this.template = template;
+	}
+	public int insert(Student student) {
+		String query= "insert into student(id,name,city) values(?,?,?)";
+		int result =this.template.update(query,student.getId(),student.getName(),student.getCity());
+		return result;
+	}
+	public int change(Student student) {
+		String query= "UPDATE student SET name =? WHERE id =?";
+		 Object[] inputs = new Object[] {student.getName(),student.getId()};
+		int result =this.template.update(query,inputs);
+		return result;
+	}
+ 
+}
